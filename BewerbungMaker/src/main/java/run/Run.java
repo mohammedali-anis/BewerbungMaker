544359d483877a0e598e12359d9c6f8ea4d0bcec
Mohammed Ali Anis <moh.ali.anis@gmail.com>
2021-12-25 00:03:08 +0100

    Projekt Architect has been created

--- BewerbungMaker/src/main/java/run/Run.java
+++ BewerbungMaker/src/main/java/run/Run.java
@@ -1,11 +1,11 @@
-package Run;
+package run;
 
 public class Run {
+
 	
 	public static void main(String[] args) {
 		
+		System.out.println("Mohammed Ali Anis");
 		
-		System.out.println("Hello Word 3");
 	}
-
 }
